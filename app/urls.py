4db770ef93c36d705508fdf3590eb90c3f58adb6
mael-donnadille <mael.donnadille@gmail.com>
2025-12-17 11:48:38 +0100

    Fonctionnel

--- app/urls.py
+++ app/urls.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include
-from core.views import home
+from django.urls import path
 from core import views
-from core.views import lee_sin_view
-from core.views import gnar_view
-from core.views import warwick_view
-from core.views import choix_perso_view
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("", views.home, name="home"),
+    path("roles/", views.roles, name="roles"),
+    path("carte/", views.carte_page, name="carte"),
+    path("conseils/", views.conseils_page, name="conseils"),
+
+
     path("vocabulaire/", views.vocabulaire, name="vocabulaire"),
-    path("lee_sin/", lee_sin_view, name="lee_sin"),
-    path("gnar/", gnar_view, name="gnar"),
-    path("warwick/", warwick_view, name="warwick"),
-    path("choix_perso/", choix_perso_view, name="choix_perso"),
+    path("choix_perso/", views.choix_perso_view, name="choix_perso"),
+
+    path("lee_sin/", views.lee_sin_view, name="lee_sin"),
+    path("gnar/", views.gnar_view, name="gnar"),
+    path("warwick/", views.warwick_view, name="warwick"),
 ]
